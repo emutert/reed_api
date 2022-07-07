2da204b00cb8b16235d0ec5696ea5e41d147b954
emutert <mutluerturk@gmail.com>
2022-07-07 18:05:07 +0100

    first full list comparison

--- compare.py
+++ compare.py
@@ -13,7 +13,7 @@ import numpy as np
 #cv will read from file
 #file upload will be available
 
-cv = my_cv.cv
+cv = my_cv.cv2
 
 #sentence tokenization
 file_docs = sent_tokenize(cv)
@@ -32,45 +32,22 @@ tfidf = TfidfModel(corpus)
 
 sims = Similarity('data/',tfidf[corpus],num_features=len(dictionary))
 
+#CV / Collected job descs average similarity percantages 
+#to-do: make this file free
+jobs = pd.read_csv('data/jobs.csv')
 
-job1_desc = """
+job_asp =[]
+for i in jobs['desc']:
+    file2_docs = sent_tokenize(i)
+    job_docs = [[w.lower() for w in word_tokenize(text)if w not in stopwords.words('english')if w.isalpha()]for text in file2_docs]
+    query_doc_bow = [dictionary.doc2bow(doc) for doc in job_docs]
 
-The Data Migration Specialist role is a multi-functional client facing role covering a variety of analytical and implementational tasks within the solutions department. You will be the point of contact between the company and the NHS, deliver and manage all aspects of data migration projects and provide support to other project members when required. You will have experience across a variety of healthcare systems and experience in a client facing role.
+    query_doc_tf_idf = tfidf[query_doc_bow]
 
-To be considered for this exciting opportunity, the successful Data Migration Specialist will require experience of;
+    sum_of_sims =(np.sum(sims[query_doc_tf_idf], dtype=np.float32))
+    job_asp.append(float(sum_of_sims / len(cv_docs)) * 100)
 
-    3-4 years of back-end healthcare systems experience.
-    Experience should ideally cover: Cerner Millennium, Epic, Silverlink PCS, DXC Platform, System C Medway.
-    Advanced SQL Data experience.
-    Strong MS Office Skills – Excel, Word, Powerpoint.
-    Good understanding of NHS Data Model
-    Work collaboratively with the team, support project members where necessary.
-    Good experience dealing with clients in a consultative way.
+jobs["asp"]=pd.to_numeric(job_asp)
 
-"""
-
-file2_docs = sent_tokenize(job1_desc)
-
-job_docs = [[w.lower() for w in word_tokenize(text)if w not in stopwords.words('english')if w.isalpha()]for text in file2_docs]
-
-#query_doc = [word_tokenize(w) for w in job1_tokens]
-query_doc_bow = [dictionary.doc2bow(doc) for doc in job_docs]
-
-query_doc_tf_idf = tfidf[query_doc_bow]
-
-sum_of_sims =(np.sum(sims[query_doc_tf_idf], dtype=np.float32))
-
-percentage_of_similarity = round(float((sum_of_sims / len(cv_docs)) * 100))
-print(f'Average similarity float: {float(sum_of_sims / len(cv_docs))}')
-print(f'Average similarity percentage: {float(sum_of_sims / len(cv_docs)) * 100}')
-print(f'Average similarity rounded percentage: {percentage_of_similarity}')
-
-
-#jobs = pd.read_csv('data/jobs.csv')
-
-
-#job_tokens =[]
-#for i in jobs['desc']:
-#    job_tokens.append([w for w in tokenizer.tokenize(i.lower()) if w not in stopwords.words('english')])
-
+jobs[jobs.asp >=35].to_csv(r'data/35andbigger.csv')
 
